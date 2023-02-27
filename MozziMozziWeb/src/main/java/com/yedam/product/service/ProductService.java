f3d0c6637ecfe6138f0dd7e6b516b2b5bd7beeec
Suna <xkq3355@naver.com>
2023-02-28 02:50:31 +0900

    2023-02-28 서나 장바구니 CRUD

--- MozziMozziWeb/src/main/java/com/yedam/product/service/ProductService.java
+++ MozziMozziWeb/src/main/java/com/yedam/product/service/ProductService.java
@@ -16,5 +16,6 @@ public interface ProductService {
 	public int addCart(CartVO cart); // 장바구니 추가
 	public CartVO checkCart(CartVO cart); // 장바구니에 있는 목록인지 체크
 	public List<CartVO> cartList(String id); // 로그인된 유저id에 따라 장바구니 목록 나오도록 만들기
-	public int modifyCount(CartVO cart); // 카트 수량 수정
+	public int modifyCount(CartVO cart); // 장바구니 수량 수정
+	public int deleteCart(int cartId); // 장바구니 삭제
 }
