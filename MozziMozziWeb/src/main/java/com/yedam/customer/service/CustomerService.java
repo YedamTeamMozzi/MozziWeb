fd5f4321f154b12e4c5c84782fcdb78364938590
PC <PC@LAPTOP>
2023-03-05 00:47:11 +0900

    0304 동군 상품문의 페이징 완료

--- MozziMozziWeb/src/main/java/com/yedam/customer/service/CustomerService.java
+++ MozziMozziWeb/src/main/java/com/yedam/customer/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.yedam.customer.service;
 import java.util.List;
 
 import com.yedam.customer.vo.NoticeVO;
+import com.yedam.customer.vo.QnaPagingVO;
 import com.yedam.customer.vo.QnaVO;
 
 public interface CustomerService {
@@ -22,4 +23,8 @@ public interface CustomerService {
 	public int addQna (QnaVO qna); //qna 등록
 	
 	public  QnaVO getQna (String qnano); //문의 단건조회
+	
+	public int countBoard(); // 게시물 총 갯수
+
+	public List<QnaPagingVO> selectBoard(QnaPagingVO vo);// 페이징 처리 게시글 조회
 }
